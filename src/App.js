57dae842688c5532b93473bac27df7298973973b
AnthonySinitsa <itonyemail@gmail.com>
2023-05-23 01:04:18 +0000

    fronend

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import BestBooks from './BestBooks';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import axios from 'axios';
+// import './App.css';
+// import Header from './Header';
+// import Footer from './Footer';
+// import BestBooks from './BestBooks';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
+
+let SERVER = process.env.REACT_APP_SERVER;
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    }
+  }
+
+  getBooks = async () => {
+    try {
+      let results = await axios.get(`${SERVER}/books`);
+      this.setState({
+        books: results.data
+      })
+    } catch (error) {
+      console.log('we have an error: ', error.response.data);
+    }
+  }
+
+  componentDidMount() {
+    this.getBooks();
+  }
+
   render() {
+    let books = this.state.books.map((book) => (
+      <p key={book._id}>{book.title} is {book.status}</p>
+    ))
     return (
       <>
-        <Router>
-          <Header />
-          <Routes>
-            <Route 
-              exact path="/"
-              element={<BestBooks />}
-            >
-            </Route>
-            {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
-          </Routes>
-          <Footer />
-        </Router>
+        <header>
+          <h1>Books</h1>
+        </header>
+        <main>
+          {
+            this.state.books.length > 0 &&
+            <>
+              {books}
+            </>
+          }
+        </main>
       </>
     )
   }
