ed0ff0875d07c6e9614f1deaad8508a337ebfb60
Tiffani Rice <tiffanirice414@gmail.com>
2023-05-26 19:27:27 -0700

    Styling Change

--- src/Header.js
+++ src/Header.js
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 class Header extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>My Favorite Books</Navbar.Brand>
+      <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+        <Navbar.Brand>See below for a collections of your favorite books!</Navbar.Brand>
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
         {/* PLACEHOLDER: render a navigation link to the about page */}
       </Navbar>
